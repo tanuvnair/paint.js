a1bf2c50da31449f441942546f55c4a9ed832d33
Tanuv Nair <tanuvnair@gmail.com>
2024-06-09 08:09:16 +0530

    Implemented Pencil Tool, Rectangle Tool and Circle Tool

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const accent = style.getPropertyValue("--accent");
 // Change toolbar active element
 var toolBar = document.getElementById("toolBar");
 var toolItems = document.getElementsByClassName("tool-item");
-
 var currentToolIndex = 0;
 
 for (var i = 0; i < toolItems.length; i++) {
@@ -27,12 +26,13 @@ for (var i = 0; i < toolItems.length; i++) {
                 console.log("Selection tool");
                 break;
             case 1:
-                console.log("Pencil tool");
                 pencilTool();
                 break;
             case 2:
-                console.log("Square tool");
-                squareTool();
+                rectangleTool();
+                break;
+            case 3:
+                circleTool();
                 break;
             default:
                 console.log("Something went wrong");
@@ -47,6 +47,22 @@ const ctx = myCanvas.getContext("2d");
 
 myCanvas.height = window.innerHeight;
 myCanvas.width = window.innerWidth;
+
+window.addEventListener("resize", updateSize);
+
+function updateSize() {
+    var savedImageData = ctx.getImageData(
+        0,
+        0,
+        myCanvas.width,
+        myCanvas.height
+    );
+
+    myCanvas.height = window.innerHeight;
+    myCanvas.width = window.innerWidth;
+
+    ctx.putImageData(savedImageData, 0, 0);
+}
 
 function pencilTool() {
     var isDrawing = false;
@@ -74,17 +90,131 @@ function pencilTool() {
         }
     });
 
-    myCanvas.addEventListener("mouseup", function () {
+    myCanvas.addEventListener("mouseup", function (e) {
         if (currentToolIndex == 1) {
             isDrawing = false;
         }
     });
 }
 
-function squareTool() {
-    myCanvas.addEventListener("mousedown", function (e) {
+function rectangleTool() {
+    var x;
+    var y;
+    var w;
+    var h;
+    var isDrawing = false;
+    var savedImageData;
 
-    })
+    myCanvas.addEventListener("mousedown", function (e) {
+        if (currentToolIndex == 2) {
+            x = e.offsetX;
+            y = e.offsetY;
+            isDrawing = true;
+
+            // Save the current canvas state
+            savedImageData = ctx.getImageData(
+                0,
+                0,
+                myCanvas.width,
+                myCanvas.height
+            );
+
+            myCanvas.addEventListener("mousemove", onMouseMove);
+        }
+    });
+
+    myCanvas.addEventListener("mouseup", function (e) {
+        if (currentToolIndex == 2) {
+            drawRect(x, y, w, h, primary);
+            isDrawing = false;
+            myCanvas.removeEventListener("mousemove", onMouseMove);
+        }
+    });
+
+    function onMouseMove(e) {
+        if (isDrawing && currentToolIndex == 2) {
+            w = e.offsetX - x;
+            h = e.offsetY - y;
+
+            // Restore the saved canvas state
+            ctx.putImageData(savedImageData, 0, 0);
+
+            drawRect(x, y, w, h, primary, false);
+        }
+    }
+}
+
+function drawRect(x, y, w, h, color, fill = true) {
+    ctx.beginPath();
+
+    if (fill == true) {
+        ctx.fillStyle = `${color}`;
+        ctx.fillRect(x, y, w, h);
+    } else {
+        ctx.strokeStyle = `${color}`;
+        ctx.strokeRect(x, y, w, h);
+    }
+}
+
+function circleTool() {
+    var x;
+    var y;
+    var r;
+    var isDrawing = false;
+    var savedImageData;
+
+    myCanvas.addEventListener("mousedown", function (e) {
+        if (currentToolIndex == 3) {
+            x = e.offsetX;
+            y = e.offsetY;
+            isDrawing = true;
+
+            // Save the current canvas state
+            savedImageData = ctx.getImageData(
+                0,
+                0,
+                myCanvas.width,
+                myCanvas.height
+            );
+
+            myCanvas.addEventListener("mousemove", onMouseMove);
+        }
+    });
+
+    myCanvas.addEventListener("mouseup", function (e) {
+        if (currentToolIndex == 3) {
+            drawCircle(x, y, r, primary);
+            isDrawing = false;
+            myCanvas.removeEventListener("mousemove", onMouseMove);
+        }
+    });
+
+    function onMouseMove(e) {
+        if (isDrawing && currentToolIndex == 3) {
+            r = Math.sqrt(
+                Math.pow(e.offsetX - x, 2) + Math.pow(e.offsetY - y, 2)
+            );
+
+            // Restore the saved canvas state
+            ctx.putImageData(savedImageData, 0, 0);
+
+            drawCircle(x, y, r, primary, false);
+        }
+    }
+}
+
+function drawCircle(x, y, radius, color, fill = true) {
+    ctx.beginPath();
+
+    if (fill == true) {
+        ctx.fillStyle = `${color}`;
+        ctx.arc(x, y, radius, 0, 2 * Math.PI);
+        ctx.fill();
+    } else {
+        ctx.strokeStyle = `${color}`;
+        ctx.arc(x, y, radius, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
 }
 
 // Clear Canvas
@@ -99,7 +229,7 @@ saveAsImageButton.addEventListener("click", function () {
     // The variable canvasUrl stores a data URL representing the image content of the canvas.
     var canvasUrl = myCanvas.toDataURL();
 
-    const dummyAnchorElement = document.createElement('a');
+    const dummyAnchorElement = document.createElement("a");
     dummyAnchorElement.href = canvasUrl;
 
     dummyAnchorElement.download = "image";
